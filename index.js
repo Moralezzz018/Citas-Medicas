0c3fa037886fbce008477979e4d5ef11917f8185
Moralezzz018 <naha_moralesa55@unicah.edu>
2025-02-27 04:31:39 +0000

    Primer Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+ require('dotenv').config();
+ const express = require('express');
+ const { Pool } = require('pg');
+ const app = express();
+ const port = process.env.PORT || 3000;
+ const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: 5432,
+ });
+app.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ message: 'Servidor funcionando', time: result.rows[0] });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+ });
+ app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+ });
