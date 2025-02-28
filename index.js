5521d2cfd2ea3754212168caf436e118eb1d9a10
LUCABOTT <lucabotteri169@gmail.com>
2025-02-28 05:27:47 +0000

    parte Luca de Post en index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  const { Pool } = require('pg');
  const app = express();
  const port = process.env.PORT || 3000;
+ app.use(express.json());
  const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -58,3 +59,75 @@ app.get('/pacientes', async (req, res) => {
   }
 });
 
+// POST PARTE LUCA //
+//                  //
+// 
+//
+
+app.post('/insertarEspecialidad', async (req, res) => {
+  const {nombre} = req.body; // Recibe datos desde el formulario
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO "especialidades" ("nombre") VALUES ($1) RETURNING *`,
+      [nombre]
+  );
+
+      res.status(201).json({ message: 'Especialidad Guardada', articulo: result.rows[0] }); // esta parte de ultimo hace un refresh de los datos que se enviaron
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+});
+
+app.post('/insertarPaciente', async (req, res) => {
+  const {nombre,edad,telefono,correo} = req.body; // Recibe datos desde el formulario
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO "pacientes" ("nombre","edad","telefono","correo") VALUES ($1, $2, $3, $4) RETURNING *`,
+      [nombre, edad, telefono, correo]
+  );
+
+      res.status(201).json({ message: 'Paciente Guardada', articulo: result.rows[0] }); // esta parte de ultimo hace un refresh de los datos que se enviaron
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+});
+
+app.post('/insertarDoctor', async (req, res) => {
+  const {nombre,id_especialidad,telefono} = req.body; // Recibe datos desde el formulario
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO "doctores" ("nombre","id_especialidad","telefono") VALUES ($1, $2, $3) RETURNING *`,
+      [nombre, id_especialidad, telefono]
+  );
+
+      res.status(201).json({ message: 'Doctor Guardada', articulo: result.rows[0] }); // esta parte de ultimo hace un refresh de los datos que se enviaron
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+});
+
+
+app.post('/insertarCita', async (req, res) => {
+  const {id_paciente,id_doctor,fecha,hora,estado} = req.body; // Recibe datos desde el formulario
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO "citas" ("id_paciente","id_doctor","fecha","hora","estado") VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+      [id_paciente,id_doctor,fecha,hora,estado]
+  );
+
+      res.status(201).json({ message: 'Cita Guardada', articulo: result.rows[0] }); // esta parte de ultimo hace un refresh de los datos que se enviaron
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+});
+
+
+// FIN PARTE LUCA //
+// 
+// 
+// 
+// //
